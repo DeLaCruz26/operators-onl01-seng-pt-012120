319e152db29f199ccea5ef97c18a9f590b22a38c
Dametrik Fick <dametrik.fick@gmail.com>
2020-01-26 21:08:26 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,13 +1,15 @@
 def unsafe?(speed)
-speed = 79
-  if "speed" > 60
+answer = unsafe?(79)
+  if answer > 60
     return true
-  elsif "speed" < 40
+answer = unsafe?(35)
+  elsif answer < 40
     return true
-  else "speed" == 50
+answer = unsafe?(50)
+  else answer == 50
     return false
   end
-end
+end 
 
 
 
